1134c62ea398cc6e303bc577927b8578faf8f4a8
tccfung31 <tccfung31@gmail.com>
2026-01-27 00:45:50 +0800

    Delete 2 directory

--- 2/video.py
+++
@@ -1,9 +0,0 @@
-from picamera2.encoders import H264Encoder, Quality
-from picamera2 import Picamera2
-import time
-picam2 = Picamera2()
-picam2.configure(picam2.create_video_configuration())
-encoder = H264Encoder()
-picam2.start_recording(encoder,'/home/pi/Desktop/test.h264',quality=Quality.HIGH)
-time.sleep(10)
-picam2.stop_recording()
